1cb91a06901cdeeb311ec2af2a422cdd58e73f2d
Vaishnavi Bansode <90685666+vaishnavipbansode24@users.noreply.github.com>
2024-01-12 16:35:04 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Array{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a[]=new int[10];
+for(int i=0;i<10;i++)
+{
+a[i]=sc.nextInt(a[i]);
+}
+}
